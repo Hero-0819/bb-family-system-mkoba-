328c3b436c1500c567d622a0afffab073e667443
shujaa <cassianshujaa06@gmail.com>
2026-08-14 03:30:14 -0400

    Fix payments app configuration

--- payments/apps.py
+++ payments/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
 
 class PaymentsConfig(AppConfig):
-    defaults_auto_filed="django.db.models.BigAutoField"
+    defaults_auto_field="django.db.models.BigAutoField"
     name="payments"
 
     def ready (self):
